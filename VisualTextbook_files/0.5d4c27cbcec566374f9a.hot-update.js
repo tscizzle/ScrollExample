a6839b987d5934bb4e1e136481c1927dbb069aab
Tyler Singer-Clark <tscizzle@mit.edu>
2018-08-18 13:06:10 -0400

    Add files via upload

---
+++ VisualTextbook_files/0.5d4c27cbcec566374f9a.hot-update.js
@@ -0,0 +1,24 @@
+webpackHotUpdate(0,{
+
+/***/ "./node_modules/css-loader/index.js?{\"importLoaders\":1}!./node_modules/postcss-loader/lib/index.js?{\"ident\":\"postcss\"}!./src/stylesheets/index.css":
+/*!***************************************************************************************************************************************!*\
+  !*** ./node_modules/css-loader?{"importLoaders":1}!./node_modules/postcss-loader/lib?{"ident":"postcss"}!./src/stylesheets/index.css ***!
+  \***************************************************************************************************************************************/
+/*! dynamic exports provided */
+/*! all exports used */
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(/*! ../../node_modules/css-loader/lib/css-base.js */ "./node_modules/css-loader/lib/css-base.js")(undefined);
+// imports
+
+
+// module
+exports.push([module.i, "body {\n  margin: 0;\n  padding: 0;\n  font-family: 'Verdana', 'Open Sans', sans-serif;\n  -webkit-font-smoothing: antialiased;\n  -moz-osx-font-smoothing: grayscale;\n  color: #fafafa;\n  background-color: #333; }\n  body svg {\n    color: #333; }\n\n.content {\n  padding: 0 0 50px; }\n\n.section-header-row {\n  margin: 20px 0; }\n\n.section-header {\n  text-align: center;\n  font-size: 2.5em;\n  font-weight: 300; }\n\n.section-subheader {\n  text-align: center;\n  margin: 10px 0 0;\n  font-size: 1.5em;\n  background-color: #333; }\n\n.svg-widget {\n  border-radius: 2px;\n  background-color: #fff;\n  -webkit-box-shadow: #bbb 0px 0px 5px;\n          box-shadow: #bbb 0px 0px 5px; }\n\n.explanation-pair {\n  display: -ms-flexbox;\n  display: flex;\n  height: 5000px;\n  padding: 0 50px; }\n\n.words-and-symbols-explanation {\n  -ms-flex-positive: 1;\n      flex-grow: 1;\n  margin: 0 0 0 30px;\n  padding: 10px 30px;\n  border-radius: 2px;\n  background-color: #444;\n  -webkit-box-shadow: #555 0px 0px 5px;\n          box-shadow: #555 0px 0px 5px;\n  overflow-y: scroll; }\n  .words-and-symbols-explanation .katex {\n    font-size: 1.4em; }\n\n.centered {\n  text-align: center; }\n\n.widget-value-setter {\n  padding: 5px 10px;\n  background-color: #333; }\n  .widget-value-setter:not(:first-child) {\n    border-left: solid 1px #aaa; }\n  .widget-value-setter.selected {\n    opacity: 0.7; }\n\n.limit-2D-epsilon-ball {\n  -webkit-transition: all 0.1s ease;\n  -o-transition: all 0.1s ease;\n  transition: all 0.1s ease; }\n\n.limit-2D-sequence-label {\n  color: #747ffc;\n  font-weight: bold; }\n", ""]);
+
+// exports
+
+
+/***/ })
+
+})
+//# sourceMappingURL=0.5d4c27cbcec566374f9a.hot-update.js.map
